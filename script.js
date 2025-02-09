e5e27c9c307acbac8bcfca67625e35717c1fb84a
akshadity01 <akshaykumaraditya01@gmail.com>
2025-02-09 15:06:20 +0530

    i am creating my friend portfolio
    hii,
    i am trying github

---
+++ script.js
@@ -0,0 +1,12 @@
+const hambgr = document.querySelector(".linecont");
+const line1 = document.querySelector(".line1");
+const line2 = document.querySelector(".line2");
+const line3 = document.querySelector(".line3");
+const navList = document.querySelector("nav ul");
+
+hambgr.addEventListener("click", () => {
+    line1.classList.toggle("line1click");
+    line2.classList.toggle("line2click");
+    line3.classList.toggle("line3click");
+    navList.classList.toggle("navlist");
+});
